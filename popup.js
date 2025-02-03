a2dbfbfabc521f76be8c66df599cd78f78ecb879
Ian-Chin <iancjsheng@gmail.com>
2025-02-03 22:57:40 +0800

    Update popup.js
    (Updated)
    
    Added sliding headers for header 1 n 2

--- popup.js
+++ popup.js
@@ -2,3 +2,22 @@ document.querySelector('.nav-toggle').addEventListener('click', function () {
     const nav = document.querySelector('.nav');
     nav.classList.toggle('open'); // Toggle the "open" class
 });
+
+
+window.addEventListener("scroll", function () {
+    const headerBar = document.querySelector(".header-bar");
+    if (window.scrollY > 50) {
+        headerBar.classList.add("scrolled");
+    } else {
+        headerBar.classList.remove("scrolled");
+    }
+});
+
+window.addEventListener("scroll", function () {
+    const headerBar2 = document.querySelector(".header-bar2");
+    if (window.scrollY > 50) {
+        headerBar2.classList.add("scrolled");
+    } else {
+        headerBar2.classList.remove("scrolled");
+    }
+});

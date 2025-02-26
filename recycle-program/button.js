cedd9cec2fba8efc2fa1db8a2484c2a52d01372f
Cheang-Zhi-Lin <cheangzhilin@gmail.com>
2025-02-26 17:01:38 +0800

    Update button.js

--- recycle-program/button.js
+++ recycle-program/button.js
@@ -1,23 +1,10 @@
+// Create button recycle_admin_main
 function show_content(id) {
     let content = document.getElementById(id);
     let currentDisplay = window.getComputedStyle(content).display;
 
     content.style.display = (currentDisplay === "none") ? "block" : "none";
 }
-
-// function create_form(id){
-//     let form = document.getElementById(id);
-//     let formShow = window.getComputedStyle(form).display;
-
-//     form.style.display = (formShow === "none") ? "block" : "none";
-// }
-
-// function delete_form(id){
-//     let dlt = document.getElementById(id);
-//     let dltShow = window.getComputedStyle(dlt).display;
-
-//     dlt.style.display = (dltShow === "none") ? "block" : "none";
-// }
 
 // On & Off Button
 function toggleDo(elementId) {

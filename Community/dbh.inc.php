1f37a5040643f7ebaa560bc857e5378252250755
Ian-Chin <iancjsheng@gmail.com>
2025-03-18 23:20:29 +0800

    Update dbh.inc.php

--- Community/dbh.inc.php
+++ Community/dbh.inc.php
@@ -1,6 +1,11 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'ecohaven');
+$servername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "ecohaven";
+
+$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
 if (!$conn){
     die("connection failed: ".mysqli_connect_error());

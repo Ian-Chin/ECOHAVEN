405f89327db1991c38c8a54dfa96d17bdf621cdb
Ian-Chin <iancjsheng@gmail.com>
2025-03-18 23:19:22 +0800

    Update commentsection.php

--- Community/commentsection.php
+++ Community/commentsection.php
@@ -26,6 +26,28 @@ function getComments($conn) {
     $result = $conn->query($sql);
 
     while ($row = $result->fetch_assoc()) {
+        $userID = $row['uid'];
+        $sql2 = "SELECT * FROM users WHERE userID='$userID'";
+        $result2 = $conn->query($sql);
+        if($row2 = $result2->fetch_assoc()){
+            echo "<div class='comment'><p>";
+            echo $row2['uid']."<br>";
+            echo $row['date']."<br>";
+            echo nl2br($row['message']);
+            echo "</p>
+                <form class='delete-form' method='POST' action='".deleteComments($conn)."'>
+                    <input type='hidden' name='cid' value='".$row['cid']."'>
+                    <button type='submit' name='commentDelete'>Delete</button>
+                </form>
+                <form class='edit-form' method='POST' action='editcomment.php'>
+                    <input type='hidden' name='cid' value='".$row['cid']."'>
+                    <input type='hidden' name='uid' value='".$row['uid']."'>
+                    <input type='hidden' name='date' value='".$row['date']."'>
+                    <input type='hidden' name='message' value='".$row['message']."'>
+                    <button>Edit</button>
+                </form>
+            </div>";
+        }
         echo "<div class='comment'><p>";
         echo $row['uid']."<br>";
         echo $row['date']."<br>";
@@ -45,6 +67,7 @@ function getComments($conn) {
         </div>";
     }
 }
+       
 
 function editComments($conn) {
     if (isset($_POST['commentSubmit'])) {
@@ -213,3 +236,4 @@ function deleteComments3($conn) {
         header("Location: community3.php" );
     }
 }
+
